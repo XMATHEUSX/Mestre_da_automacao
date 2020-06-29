7c306c507ec0e2ca8f25995a1af9af0e277de9c8
Matheus Xavier <matheusxavier2302@gmail.com>
2020-06-28 23:54:14 -0300

    Simulador_de_dado

---
+++ Módulo 2/simulador_de_dado.py
@@ -0,0 +1,40 @@
+import random
+
+
+class dado:
+    def __init__(self, lados):
+        self.lados = lados
+
+    def jogar_dados(self):
+        while True:
+            print(random.randrange(1, (self.lados)+1))
+            while True:
+                continuar = input("Continuar jogando ? (S/N)")
+                if continuar == "N" or continuar == "n":
+                    break
+                elif continuar == "S" or continuar == "s":
+                    break
+                else:
+                    print("***Entrada  incorreta***\n ")
+            if continuar == "N" or continuar == "n":
+                break
+        print("os dados foram deixados de lado")
+
+
+dado_20 = dado(20)
+dado_20.jogar_dados()
+'''
+while True:
+    Pergunta = input("Qual a sua Dúvida ? ")
+    while True:
+        continuar = input("Você possui mais dúvidas ? (S/N)")
+        if continuar == "N" or continuar == "n":
+            break
+        elif continuar == "S" or continuar == "s":
+            break
+        else:
+            print("***Entrada  incorreta***\n ")
+    if continuar == "N" or continuar == "n":
+        break
+print("Obrigado Por Usar O Programa")
+'''
