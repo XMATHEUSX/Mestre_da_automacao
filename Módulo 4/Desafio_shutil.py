66e6ffbd728a0183287bf610615581303b4fd737
Matheus Xavier <matheusxavier2302@gmail.com>
2020-07-08 19:10:33 -0300

    arrumando

--- Módulo 4/Desafio_shutil.py
+++ Módulo 4/Desafio_shutil.py
@@ -1,27 +1,40 @@
 import os
 import shutil
 
-shutil.copy(os.getcwd()+os.sep+'bacon.txt',os.getcwd()+os.sep+'Copia')
-shutil.copytree(os.getcwd()+os.sep+'desafios texto',os.getcwd()+os.sep+'Copia')
-shutil.move(os.getcwd()+os.sep+'bacon.txt', os.getcwd()+os.sep+'Fotos')
-shutil.rmtree(os.getcwd()+os.sep+'Copia')
-shutil.make_archive('bacon','zip',os.getcwd()+os.sep)
-shutil.unpack_archive('bacon.zip','bacon')
+# shutil.copy(os.getcwd()+os.sep+'Módulo 4'+os.sep +
+#             'arquivos.txt', os.getcwd()+os.sep+'Copia')
+
+# shutil.copytree(os.getcwd()+os.sep+'Módulo 4'+os.sep + 'desafios texto',
+#                 os.getcwd()+os.sep+'Módulo 4'+os.sep+'Copia_diretorio')
+
+# shutil.move(os.getcwd()+os.sep+'Módulo 4'+os.sep +
+#             'arquivos.txt', os.getcwd()+os.sep+'Módulo 4' + os.sep + 'Fotos')
+
+# shutil.rmtree(os.getcwd()+os.sep+'Módulo 4'+os.sep + 'Copia_diretorio')
+
+# shutil.make_archive('bacon', 'zip', os.getcwd()+os.sep+'Módulo 4')
+
+# shutil.unpack_archive('bacon.zip', 'bacon')
 
 
 # Desafio 1
-shutil.copy(os.getcwd()+os.sep+'nomes.txt',os.getcwd()+os.sep+'Arquivos 2010')
+shutil.copy(os.getcwd()+os.sep+'Módulo 4'+os.sep+'nomes.txt',
+            os.getcwd()+os.sep+'Módulo 4'+os.sep+'Arquivos 2010')
 # Desafio 2
-shutil.move(os.getcwd()+os.sep+'inscrições.pdf',os.getcwd()+os.sep+'Documentação')
+# shutil.move(os.getcwd()+os.sep+'Módulo 4'+os.sep+'inscrições.pdf',
+#             os.getcwd()+os.sep+'Módulo 4'+os.sep+'Documentação')
 # Desafio 3
-shutil.copytree(os.getcwd()+os.sep+'Arquivos 2010',
-                os.getcwd()+os.sep+'Backup Arquivos 2010')
+# shutil.copytree(os.getcwd()+os.sep+'Módulo 4'+os.sep+'Arquivos 2010',
+#                 os.getcwd()+os.sep+'Módulo 4'+os.sep+'Backup Arquivos 2010')
 # Desafio 4
-shutil.make_archive('Documentação', 'zip', os.getcwd()+os.sep+'Documentação')
+# shutil.make_archive(os.getcwd()+os.sep+'Módulo 4'+os.sep+'os.sep'+ 'Documentação',
+#                     'zip', os.getcwd()+os.sep+'Módulo 4'+os.sep+'Documentação')
 # Desafio 5
-shutil.move(os.getcwd()+os.sep+'Documentação.zip',os.getcwd()+os.sep+'Backup')
+# shutil.move(os.getcwd()+os.sep+'Módulo 4'+os.sep +
+#             'Documentação.zip', os.getcwd()+os.sep+'Módulo 4'+os.sep+'Backup')
 # Desafio 6
-shutil.rmtree(os.getcwd()+os.sep+'Arquivos 2010')
-shutil.rmtree(os.getcwd()+os.sep+'Documentação')
+# shutil.rmtree(os.getcwd()+os.sep+'Módulo 4'+os.sep+'Arquivos 2010')
+# shutil.rmtree(os.getcwd()+os.sep+'Módulo 4'+os.sep+'Documentação')
 # Desafio 7
-shutil.make_archive('Backup Arquivos Python','zip',os.getcwd()+os.sep)
+# shutil.make_archive('Backup Arquivos Python', 'zip',
+#                     os.getcwd()+os.sep+'Módulo 4'+os.sep)
---
+++ bacon/Criando_Diretorios.py
@@ -0,0 +1,6 @@
+import os
+
+
+os.mkdir('Módulo 4'+os.sep+'Arquivos')
+os.makedirs('Módulo 4'+os.sep+'Arquivos'+os.sep+'Arquivos pdf')
+os.makedirs('Módulo 4'+os.sep+'Fotos'+os.sep+'Fotos Verão')
